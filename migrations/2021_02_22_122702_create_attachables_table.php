d64bbf2b015e4b7595e098d40511374249d19a49
Marko Dupor <Norgul@users.noreply.github.com>
2021-03-18 15:13:58 +0100

    Git actions + fix (#3)

--- migrations/2021_02_22_122702_create_attachables_table.php
+++ migrations/2021_02_22_122702_create_attachables_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAttachableTable extends Migration
+class CreateAttachablesTable extends Migration
 {
     /**
      * Run the migrations.
