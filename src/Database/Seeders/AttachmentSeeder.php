0aa0ee589e89f38debd04173fc7e7a00d3054b1d
Marko Dupor <Norgul@users.noreply.github.com>
2021-03-18 15:51:58 +0100

    Fix #patch

--- src/Database/Seeders/AttachmentSeeder.php
+++ src/Database/Seeders/AttachmentSeeder.php
@@ -7,7 +7,7 @@ namespace Asseco\Attachments\Database\Seeders;
 use Asseco\Attachments\App\Models\Attachment;
 use Illuminate\Database\Seeder;
 
-class AttachmentsSeeder extends Seeder
+class AttachmentSeeder extends Seeder
 {
     public function run(): void
     {
