483375562e1248ca31790c079b83df1b9e84c6cd
Marko Dupor <Norgul@users.noreply.github.com>
2021-03-22 11:30:04 +0000

    Apply fixes from StyleCI

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('api')
     ->middleware('api')
     ->group(function () {
-
         Route::apiResource('attachments', AttachmentController::class)->except(['update']);
-
     });
