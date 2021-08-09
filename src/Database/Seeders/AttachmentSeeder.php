9750b54251cb18219ab5d6697e018aadccbbf83e
Marko Dupor <marko.dupor@asseco-see.hr>
2021-08-09 14:38:55 +0200

    Seeder fix

--- src/Database/Seeders/AttachmentSeeder.php
+++ src/Database/Seeders/AttachmentSeeder.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 namespace Asseco\Attachments\Database\Seeders;
 
-use Asseco\Attachments\App\Models\Attachment;
+use Asseco\Chassis\App\Models\Attachment;
 use Illuminate\Database\Seeder;
 
 class AttachmentSeeder extends Seeder
 {
     public function run(): void
     {
-        Attachment::factory()->count(50)->create();
+        /** @var Attachment $attachment */
+        $attachment = app(Attachment::class);
+
+        $attachment::factory()->count(50)->create();
     }
 }
